6430d2bb6430e12abb56a1520a90fd5d2fd90d8e
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2019-10-14 21:10:26 -0300

    Criação do primeiro teste unitário do BaseArchiveGenerator

--- Archive.Generator.Tests/BaseArchiveGeneratorTests.cs
+++ Archive.Generator.Tests/BaseArchiveGeneratorTests.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Archive.Generator.Tests.FakeObjects;
+using Xunit;
 
 namespace Archive.Generator.Tests
 {
-    class BaseArchiveGeneratorTests
+    public class BaseArchiveGeneratorTests
     {
+        [Fact]
+        public void FakeObject1()
+        {
+            var stringFakeObject1 = "         1valor da prop 2     201706240000012500valor para s";
+
+            var fakeObject1 = new FakeObject1
+            {
+                Property1 = 1,
+                Property2 = "valor da prop 2",
+                Property3 = new DateTime(2017, 06, 24),
+                Property4 = (decimal)12.5,
+                Property5 = "valor para ser cortado"
+            };
+
+            Assert.Equal(stringFakeObject1, fakeObject1.GenerateString());
+        }
     }
 }
--- Archive.Generator.Tests/FakeObjects/FakeObject1.cs
+++ Archive.Generator.Tests/FakeObjects/FakeObject1.cs
@@ -3,7 +3,30 @@ using System;
 
 namespace Archive.Generator.Tests.FakeObjects
 {
-    public class FakeObject1
+    public class FakeObject1 : BaseArchiveGenerator
     {
+        [Order(1)]
+        [Size(10)]
+        [Int(' ')]
+        public int Property1 { get; set; }
+
+        [Order(2)]
+        [Size(20)]
+        [String(true)]
+        public string Property2 { get; set; }
+
+        [Order(3)]
+        [DateTimeFormatter("yyyyMMdd")]
+        public DateTime Property3 { get; set; }
+
+        [Order(4)]
+        [Size(10)]
+        [Decimal(3, '0')]
+        public decimal Property4 { get; set; }
+
+        [Order(5)]
+        [Size(12)]
+        [String(false)]
+        public string Property5 { get; set; }
     }
 }
